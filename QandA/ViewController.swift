40ba5dccc9e94b1a6d33ea5e2e62b99e4122aaa7
Tim <timisveryhandsome@gmail.com>
2018-12-03 13:49:32 +0800

    Initial Commit

---
+++ QandA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QandA
+//
+//  Created by Tim Syu on 2018/12/3.
+//  Copyright © 2018 Tim Syu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
